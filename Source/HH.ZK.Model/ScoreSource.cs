5dece12babc3934d12be07831dade7b9a997ab6c
lijianhua <233955501@qq.com>
2025-09-30 10:35:15 +0800

    1

--- Source/HH.ZK.Model/ScoreSource.cs
+++ Source/HH.ZK.Model/ScoreSource.cs
@@ -9,13 +9,10 @@ namespace HH.ZK.Model
     public enum ScoreSource
     {
         文件导入 = 0,
-        汇海主机 = 1,
-        手工录入 = 2,
-        汇海APP = 3,
-        电动计时 = 4,
-        IC卡=5,
+        主机 = 1,
+        AI设备 = 2,
+        手工录入 = 9,
         其它设备 = 10,
         系统生成 = 11,
-        复议生成 = 12,
     }
 }
